03f6fac7a8c8bdb694ef6ca89d49e19668b6bdd8
Troy Edwards <tntjedi.2006@gmail.com>
2024-09-13 16:52:04 -0700

    add stuff to backend type and wayland backend

--- src/lib/tuck_backends/tuck_backends.ml
+++ src/lib/tuck_backends/tuck_backends.ml
@@ -1,7 +1,8 @@
 (* TODO: what does a backend need to do and have? *)
 module type backend = sig
     val accept_frame : Tuck.Buffer.t -> unit
+    val rebuild_zone : int -> int -> Tuck.Buffer.t -> unit (* might only work with wayland? *)
 end
 
 (* TODO: constrain to backend type when ready *)
-module Wayland = Tuck_wayland
+module Wayland : backend = Tuck_wayland
---
+++ src/lib/tuck_backends/tuck_wayland/tuck_wayland.ml
@@ -0,0 +1,5 @@
+let accept_frame (_buf : Tuck.Buffer.t) : unit =
+    ()
+
+let rebuild_zone (_x : int) (_y: int) (_buf : Tuck.Buffer.t) : unit =
+    ()
