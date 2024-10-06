9fb21096de924393834208278e7161a45fffbbce
Troy Edwards <tntjedi.2006@gmail.com>
2024-10-06 15:35:40 -0700

    refactor App.main and Start_app to use Tuck_env.t

--- src/lib/tuck/tuck.ml
+++ src/lib/tuck/tuck.ml
@@ -1,14 +1,29 @@
 open Eio.Std
+open Wayland
 
 module Buffer = Buffer
 module Tuck_env = Tuck_env
 
 module type App = sig
-    val main : Eio_unix.Stdenv.base -> Eio.Switch.t -> unit
+    val main : tuck_env:Tuck_env.t -> unit
 end
 
 (* TODO make this event-loop agnostic? *)
 module Start_app (A : App) = struct
-    let () = Eio_main.run @@ fun env -> Switch.run @@ fun sw -> A.main env sw
+    let () =
+        Eio_main.run @@ fun env ->
+            Switch.run @@ fun sw ->
+                let transport = Unix_transport.connect ~sw ~net:env#net () in
+                let display = Client.connect ~sw transport in
+                let reg = Registry.of_display display in
+                let compositor =
+                    Registry.bind reg (new Wayland_client.Wl_compositor.v4) in
+                let tuck_env : Tuck_env.t = {
+                    eio_env = env;
+                    switch = sw;
+                    reg;
+                    compositor;
+                } in
+                A.main ~tuck_env
 end
 
